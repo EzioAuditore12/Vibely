67e8fcef4b67b92c2549c64dc4f9d919f2bf8e76
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-01 13:18:39 +0530

    added logo to navbar

---
+++ frontEnd/src/components/Navbar/01_Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+        <h1 className='text-2xl text-start m-3'>Vibely</h1>
+    </div>
+  )
+}
+
+export default Logo
---
+++ frontEnd/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './01_Logo/Logo'
+function Navbar() {
+ return (
+    <nav>
+        <div className='w-full h-9 flex'>
+            <Logo/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
